d9f8f63878bdec2c55bec4753a4811f17943e9df
xamionex <percesvrzic@gmail.com>
2021-12-01 08:12:21 +0100

    unify scripts

--- swup.js
+++
@@ -1,1 +0,0 @@
-const swup = new Swup(); // only this line when included with script tag
--- typewriter.js
+++
@@ -1,8 +0,0 @@
-new Typewriter('#myname', {
-    strings: ['Hello, I\'m <strong>Petar</strong>', 'I\'m a <strong>Serbian Programmer</strong>', 'I hope you like my site'],
-    autoStart: true,
-    loop: true,
-    deleteSpeed: 3,
-    cursor: '!',
-    delay: 100,
-  });
