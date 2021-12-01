acc86a1b41fb7e1ec07253b1994b73a2f7da58d1
xamionex <percesvrzic@gmail.com>
2021-12-01 07:22:38 +0100

    typewriter :D

---
+++ typewriter.js
@@ -0,0 +1,8 @@
+new Typewriter('#myname', {
+    strings: ['Hello, I\'m <strong>Petar</strong>', 'I\'m a <strong>Serbian Programmer</strong>', 'I hope you like my site'],
+    autoStart: true,
+    loop: true,
+    deleteSpeed: 3,
+    cursor: '!',
+    delay: 85,
+  });
