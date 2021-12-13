270c222bbf81279754a9ae9c09acb1ca7f558964
xamionex <percesvrzic@gmail.com>
2021-12-13 22:24:44 +0100

    omg buttones

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,7 @@ module.exports = {
       },
       fontFamily: {
         londrinashadow: ['Londrina Shadow'],
+        solway: ['Solway'],
         josefinsans: ['Josefin Sans'],
         alegreyasans: ['Alegreya Sans']
       }
