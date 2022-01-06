31a72757cd41010c7439a001247ce131a5907aad
amione <percesvrzic@gmail.com>
2022-01-06 22:48:57 +0100

    remove swupheadplugin, replace confetti.js

--- assets/script.js
+++ assets/script.js
@@ -4,12 +4,10 @@ const swup = new Swup({
     color: '#2D2E82',
     duration: 300,
     direction: 'to-right',
-  }), new SwupHeadPlugin(), new SwupBodyClassPlugin()]
-}); // only this line when included with script tag
-
-// run once
+  }), new SwupBodyClassPlugin()]
+});
+const jsConfetti = new JSConfetti()
 init();
-// this event runs for every page view after initial load
 swup.on('contentReplaced', init);
 function init() {
   if (document.querySelector('#myname')) {
@@ -48,47 +46,49 @@ function init() {
   if (document.querySelector('#ago')) {
     (function () {
       const second = 1000,
-        minute = second * 60,
-        hour = minute * 60,
-        day = hour * 24;
+      minute = second * 60,
+      hour = minute * 60,
+      day = hour * 24;
       let today = new Date(),
-        dd = String(today.getDate()).padStart(2, "0"),
-        mm = String(today.getMonth() + 1).padStart(2, "0"),
-        yyyy = today.getFullYear(),
+      dd = String(today.getDate()).padStart(2, "0"),
+      mm = String(today.getMonth() + 1).padStart(2, "0"),
+      yyyy = today.getFullYear(),
       nextYear = yyyy + 1,
-        dayMonth = "11/22/",
-        birthday = dayMonth + yyyy;
+      dayMonth = "1/6/",
+      birthday = dayMonth + yyyy;
       today = mm + "/" + dd + "/" + yyyy;
       if (today > birthday) {
         birthday = dayMonth + nextYear;
       }
       const countDown = new Date(birthday).getTime(),
-        x = setInterval(function () {
-          const now = new Date().getTime(),
-            distance = countDown - now;
-            if (document.querySelector('#countdown')) {
-              document.getElementById("ago").innerText = 365 - Math.floor(distance / (day))
-              document.getElementById("days").innerText = Math.floor(distance / (day)),
-              document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+      x = setInterval(function () {
+        const now = new Date().getTime(),
+        distance = countDown - now;
+        if (document.querySelector('#countdown')) {
+          document.getElementById("ago").innerText = 365 - Math.floor(distance / (day))
+          document.getElementById("days").innerText = Math.floor(distance / (day)),
+          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
               document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
               document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second)
             };
-          //do something later when date is reached
-          if (distance < 0) {
-            if (document.querySelector('#countdown')) {
-              document.getElementById("announcer").innerText = "It's my birthday!"
-              document.getElementById("announcer").classList.add("text-6xl")
-              document.getElementById("countdown").style.display = "none"
-              document.getElementById("content").style.display = "block"
-              document.getElementById("announcer2").style.display = "none"
-            };
-            const script = document.createElement('script');
-            script.src = '/assets/confetti.js';
-            document.head.appendChild(script);
-            clearInterval(x);
-          }
-          //seconds
-        }, 0)
-    }());
-  }
-}
+            if (distance < 0) {
+              if (document.querySelector('#countdown')) {
+                document.getElementById("announcer").innerText = "It's my birthday!"
+                document.getElementById("announcer").classList.add("text-6xl")
+                document.getElementById("countdown").style.display = "none"
+                document.getElementById("content").style.display = "block"
+                document.getElementById("announcer2").style.display = "none"
+              };
+              jsConfetti.addConfetti({
+                emojis: ['🌈', '⚡️', '💥', '✨', '💫', '🌸'],
+                confettiColors: ['#ff0a54', '#ff477e', '#ff7096', '#ff85a1', '#fbb1bd', '#f9bec7',],
+                confettiRadius: 6,
+                confettiNumber: 200,
+                emojiSize: 50,
+              })
+              clearInterval(x);
+            }
+          }, 0)
+        }());
+      }
+    }
