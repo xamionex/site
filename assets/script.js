a5e5fb07192ca559639b3be09cd1650f2f53c39e
amione <percesvrzic@gmail.com>
2022-01-28 01:34:40 +0100

    remove xmlns, connect social buttons

--- assets/script.js
+++ assets/script.js
@@ -9,7 +9,37 @@ const swup = new Swup({
 const jsConfetti = new JSConfetti()
 init();
 swup.on('contentReplaced', init);
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /*loop through a collection of all HTML elements:*/
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("include-html");
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("include-html");
+          includeHTML();
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*exit the function:*/
+      return;
+    }
+  }
+};
 function init() {
+  if (document.querySelector('#socials')) {
+    includeHTML()
+  }
   if (document.querySelector('#myname')) {
     new Typewriter('#myname', {
       strings: ['Hello, I\'m <strong>Petar</strong>', 'I\'m a <strong>Serbian Programmer</strong>', 'I hope you like my site.'],
