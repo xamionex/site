3df3bee07b1aae26a6f668fd00f49080da81c044
amione <percesvrzic@gmail.com>
2022-03-08 17:16:24 +0100

    add random cursor, update js dependencies

--- assets/dependencies.js
+++ assets/dependencies.js
@@ -1,3 +1,6 @@
+addCss('/assets/style.css');
+intrand = Math.floor(Math.random() * 5);
+chooseCursor(intrand)
 function addCss(fileName) {
     var head = document.head;
     var link = document.createElement("link");
@@ -5,16 +8,22 @@ function addCss(fileName) {
     link.rel = "stylesheet";
     link.href = fileName;
     head.appendChild(link);
-  }
-  addCss('/assets/style.css');
-  load_script = function(src) {
-      // Initialize scripts queue
-      if( load_script.scripts === undefined ) {
-          load_script.scripts = [];
+} // adds css to head
+function chooseCursor(intrand) {
+    if (intrand == 0){addCss('/assets/cursor/FlameSword.css')}
+    else if (intrand == 1){addCss('/assets/cursor/FlamingArrowGlitterPurple.css')}
+    else if (intrand == 2){addCss('/assets/cursor/LightningElectricity.css')}
+    else if (intrand == 3){addCss('/assets/cursor/LightsOfHeaven.css')}
+    else if (intrand == 4){addCss('/assets/cursor/TurqouiseElectricityLightning.css')}
+} // yandere dev style choosing of cursor :trol:
+load_script = function (src) {
+    // Initialize scripts queue
+    if (load_script.scripts === undefined) {
+        load_script.scripts = [];
         load_script.index = -1;
         load_script.loading = false;
-        load_script.next = function() {
-            if( load_script.loading ) return;
+        load_script.next = function () {
+            if (load_script.loading) return;
             // Load the next queue item
             load_script.loading = true;
             var item = load_script.scripts[++load_script.index];
@@ -25,27 +34,38 @@ function addCss(fileName) {
             // When complete, start next item in queue and resolve this item's promise
             script.onload = () => {
                 load_script.loading = false;
-                if( load_script.index < load_script.scripts.length - 1 ) load_script.next();
+                if (load_script.index < load_script.scripts.length - 1) load_script.next();
                 item.resolve();
             };
             head.appendChild(script);
         };
     };
     // Adding a script to the queue
-    if( src ) {
+    if (src) {
         // Check if already added
-        for(var i=0; i < load_script.scripts.length; i++) {
-            if( load_script.scripts[i].src == src ) return load_script.scripts[i].promise;
+        for (var i = 0; i < load_script.scripts.length; i++) {
+            if (load_script.scripts[i].src == src) return load_script.scripts[i].promise;
         }
         // Add to the queue
-        var item = { src: src };
-        item.promise = new Promise(resolve => {item.resolve = resolve;});
+        var item = {
+            src: src
+        };
+        item.promise = new Promise(resolve => {
+            item.resolve = resolve;
+        });
         load_script.scripts.push(item);
         load_script.next();
     }
 
     // Return the promise of the last queue item
-    return load_script.scripts[ load_script.scripts.length - 1 ].promise;
+    return load_script.scripts[load_script.scripts.length - 1].promise;
 };
 
-['https://unpkg.com/typewriter-effect@latest/dist/core.js', 'https://storage.ko-fi.com/cdn/scripts/overlay-widget.js', 'https://unpkg.com/swup@latest/dist/swup.min.js', '/assets/SwupOverlayTheme.min.js', '/assets/SwupBodyClassPlugin.min.js', 'https://cdn.jsdelivr.net/npm/js-confetti@latest/dist/js-confetti.browser.js', '/assets/script.js'].forEach(load_script);
+['https://unpkg.com/typewriter-effect@latest/dist/core.js',
+    'https://storage.ko-fi.com/cdn/scripts/overlay-widget.js',
+    'https://unpkg.com/swup@latest/dist/swup.min.js',
+    'https://cdn.jsdelivr.net/gh/swup/overlay-theme@latest/dist/SwupOverlayTheme.min.js',
+    'https://cdn.jsdelivr.net/gh/swup/body-class-plugin@latest/dist/SwupBodyClassPlugin.min.js',
+    'https://cdn.jsdelivr.net/npm/js-confetti@latest/dist/js-confetti.browser.js',
+    '/assets/script.js'
+].forEach(load_script);
